8753970ca946fdd8b272b8469e1ef12900149ffc
Denis1325 <penkovdenis271@gmail.com>
2026-03-20 21:40:38 +0300

    solution 1

--- solutions/sem02/lesson05/task3.py
+++ solutions/sem02/lesson05/task3.py
@@ -9,4 +9,20 @@ def adaptive_filter(
     Vs: np.ndarray,
     Vj: np.ndarray,
     diag_A: np.ndarray,
-) -> np.ndarray: ...
+) -> np.ndarray:
+    M_Vs, N = Vs.shape
+    M_Vj, K = Vj.shape
+
+    if M_Vs != M_Vj:
+        raise ShapeMismatchError
+    if diag_A.shape[0] != K:
+        raise ShapeMismatchError
+
+    Vj_H = Vj.conj().T
+    A = np.diag(diag_A)
+
+    R = np.eye(M_Vs, dtype=complex) + Vj @ A @ Vj_H
+
+    y = np.linalg.inv(R) @ Vs
+
+    return y
